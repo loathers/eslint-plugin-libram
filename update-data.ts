983f2ad607accabcecff220c816e501bda2ebcca
Sam Gaus <sam@gaus.co.uk>
2023-10-24 17:34:31 +0100

    Totally rely on data-of-loathing and simplify data update massively as a result

--- update-data.ts
+++ update-data.ts
@@ -1,86 +1,62 @@
-import fs from "fs";
+import fs from "fs/promises";
 import {
+  disambiguate,
   loadClasses,
   loadEffects,
   loadFamiliars,
   loadItems,
+  loadLocations,
+  loadMonsters,
   loadPaths,
   loadSkills,
 } from "data-of-loathing";
 
-async function getContents(url: string) {
-  const response = await fetch(url);
-  return response.text();
-}
+type JobType = {
+  filename: string;
+  loader: () => Promise<{ data: { id: number; name: string }[] }>;
+  shouldDisambiguate: boolean;
+};
 
-async function getMafiaData(path: string) {
-  const text = await getContents(
-    `https://raw.githubusercontent.com/kolmafia/kolmafia/main/src/data/${path}`,
-  );
-  return text
-    .split("\n")
-    .slice(1)
-    .filter((line) => line[0] !== "#")
-    .map((line) => line.split("\t"));
-}
+const jobs: JobType[] = [
+  { filename: "classes.json", loader: loadClasses, shouldDisambiguate: false },
+  { filename: "effects.json", loader: loadEffects, shouldDisambiguate: false },
+  {
+    filename: "familiars.json",
+    loader: loadFamiliars,
+    shouldDisambiguate: false,
+  },
+  { filename: "items.json", loader: loadItems, shouldDisambiguate: true },
+  {
+    filename: "locations.json",
+    loader: loadLocations,
+    shouldDisambiguate: false,
+  },
+  { filename: "monsters.json", loader: loadMonsters, shouldDisambiguate: true },
+  { filename: "paths.json", loader: loadPaths, shouldDisambiguate: false },
+  { filename: "skills.json", loader: loadSkills, shouldDisambiguate: false },
+];
 
-function disambiguate(entities: { id: number; name: string }[]) {
-  const nameIdsMap = new Map<string, number[]>();
-  for (const { id, name } of entities) {
-    if (!name || name.trim() === "") continue;
-    let ids = nameIdsMap.get(name);
-    if (!ids) {
-      ids = [];
-      nameIdsMap.set(name, ids);
-    }
-    ids.push(id);
+async function exists(file: string) {
+  try {
+    await fs.access(file);
+    return true;
+  } catch {
+    return false;
   }
-
-  return ([] as string[]).concat(
-    ...Array.from(nameIdsMap).map(([name, ids]) =>
-      ids.length === 1 ? [name] : ids.map((id) => `[${id}]${name}`),
-    ),
-  );
-}
-
-async function loadLocations() {
-  const locationLines = await getMafiaData("adventures.txt");
-  return locationLines.map((line) => line[3]).filter((name) => name);
-}
-
-async function loadMonsters() {
-  return (await getMafiaData("monsters.txt")).map(([name, id]) => ({
-    id: Number(id),
-    name,
-  }));
 }
 
 async function main() {
-  if (!fs.existsSync("data")) fs.mkdirSync("data");
+  if (!(await exists("data"))) await fs.mkdir("data");
 
-  const effects = (await loadEffects()).data.map((e) => e.name);
-  fs.writeFileSync("data/effects.json", JSON.stringify(effects));
-
-  const familiars = (await loadFamiliars()).data.map((f) => f.name);
-  fs.writeFileSync("data/familiars.json", JSON.stringify(familiars));
-
-  const items = disambiguate((await loadItems()).data);
-  fs.writeFileSync("data/items.json", JSON.stringify(items));
-
-  const locations = await loadLocations();
-  fs.writeFileSync("data/locations.json", JSON.stringify(locations));
-
-  const monsters = disambiguate(await loadMonsters());
-  fs.writeFileSync("data/monsters.json", JSON.stringify(monsters));
-
-  const skills = (await loadSkills()).data.map((s) => s.name);
-  fs.writeFileSync("data/skills.json", JSON.stringify(skills));
-
-  const classes = (await loadClasses()).data.map((c) => c.name);
-  fs.writeFileSync("data/classes.json", JSON.stringify(classes));
-
-  const paths = (await loadPaths()).data.map((p) => p.name);
-  fs.writeFileSync("data/paths.json", JSON.stringify(paths));
+  await Promise.all(
+    jobs.map(async ({ filename, loader, shouldDisambiguate }) => {
+      const { data } = await loader();
+      const names = shouldDisambiguate
+        ? disambiguate(data)
+        : data.map((d) => d.name);
+      return await fs.writeFile(`data/${filename}`, JSON.stringify(names));
+    }),
+  );
 }
 
 main();
