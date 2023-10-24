4a0347491d1e4fdab8246a92c97fe5ae18a1c9c0
Sam Gaus <sam@gaus.co.uk>
2023-10-24 13:58:16 +0100

    Update to use data-of-loathing

--- update-data.ts
+++ update-data.ts
@@ -1,4 +1,12 @@
 import fs from "fs";
+import {
+  loadClasses,
+  loadEffects,
+  loadFamiliars,
+  loadItems,
+  loadPaths,
+  loadSkills,
+} from "data-of-loathing";
 
 async function getContents(url: string) {
   const response = await fetch(url);
@@ -7,43 +15,18 @@ async function getContents(url: string) {
 
 async function getMafiaData(path: string) {
   const text = await getContents(
-    `https://raw.githubusercontent.com/kolmafia/kolmafia/main/src/data/${path}`
+    `https://raw.githubusercontent.com/kolmafia/kolmafia/main/src/data/${path}`,
   );
   return text
     .split("\n")
     .slice(1)
-    .filter((line) => line[0] !== "#");
+    .filter((line) => line[0] !== "#")
+    .map((line) => line.split("\t"));
 }
 
-async function getMafiaEnumData(filename: string) {
-  const text = await getContents(
-    `https://raw.githubusercontent.com/kolmafia/kolmafia/main/src/net/sourceforge/kolmafia/${filename}.java`
-  );
-  const enumStart = text.indexOf("public enum");
-  const enumEnd = text.indexOf(";", enumStart);
-  return text.slice(enumStart, enumEnd);
-}
-
-const ENUM_NAME_PATTERN = /\(\n*\s*"([^"]+)",/g;
-
-function matchAll(text: string, pattern: RegExp) {
-  const matches: string[] = [];
-
-  let match: RegExpExecArray | null;
-  while ((match = pattern.exec(text)) !== null) {
-    matches.push(match[1]);
-  }
-
-  return matches;
-}
-
-function disambiguate(
-  lines: string[],
-  componentsToIdName: (components: string[]) => [number, string]
-) {
+function disambiguate(entities: { id: number; name: string }[]) {
   const nameIdsMap = new Map<string, number[]>();
-  for (const line of lines) {
-    const [id, name] = componentsToIdName(line.split("\t"));
+  for (const { id, name } of entities) {
     if (!name || name.trim() === "") continue;
     let ids = nameIdsMap.get(name);
     if (!ids) {
@@ -55,56 +38,48 @@ function disambiguate(
 
   return ([] as string[]).concat(
     ...Array.from(nameIdsMap).map(([name, ids]) =>
-      ids.length === 1 ? [name] : ids.map((id) => `[${id}]${name}`)
-    )
+      ids.length === 1 ? [name] : ids.map((id) => `[${id}]${name}`),
+    ),
   );
+}
+
+async function loadLocations() {
+  const locationLines = await getMafiaData("adventures.txt");
+  return locationLines.map((line) => line[3]).filter((name) => name);
+}
+
+async function loadMonsters() {
+  return (await getMafiaData("monsters.txt")).map(([name, id]) => ({
+    id: Number(id),
+    name,
+  }));
 }
 
 async function main() {
   if (!fs.existsSync("data")) fs.mkdirSync("data");
 
-  const effectLines = await getMafiaData("statuseffects.txt");
-  const effects = disambiguate(effectLines, ([id, name]) => [
-    parseInt(id),
-    name,
-  ]);
+  const effects = (await loadEffects()).data.map((e) => e.name);
   fs.writeFileSync("data/effects.json", JSON.stringify(effects));
 
-  const familiarLines = await getMafiaData("familiars.txt");
-  const familiars = familiarLines
-    .map((line) => line.split("\t")[1])
-    .filter((name) => name);
+  const familiars = (await loadFamiliars()).data.map((f) => f.name);
   fs.writeFileSync("data/familiars.json", JSON.stringify(familiars));
 
-  const itemLines = await getMafiaData("items.txt");
-  const items = disambiguate(itemLines, ([id, name]) => [parseInt(id), name]);
+  const items = disambiguate((await loadItems()).data);
   fs.writeFileSync("data/items.json", JSON.stringify(items));
 
-  const locationLines = await getMafiaData("adventures.txt");
-  const locations = locationLines
-    .map((line) => line.split("\t")[3])
-    .filter((name) => name);
+  const locations = await loadLocations();
   fs.writeFileSync("data/locations.json", JSON.stringify(locations));
 
-  const monsterLines = await getMafiaData("monsters.txt");
-  const monsters = disambiguate(monsterLines, ([name, id]) => [
-    parseInt(id),
-    name,
-  ]);
+  const monsters = disambiguate(await loadMonsters());
   fs.writeFileSync("data/monsters.json", JSON.stringify(monsters));
 
-  const skillLines = await getMafiaData("classskills.txt");
-  const skills = skillLines
-    .map((line) => line.split("\t")[1])
-    .filter((name) => name);
+  const skills = (await loadSkills()).data.map((s) => s.name);
   fs.writeFileSync("data/skills.json", JSON.stringify(skills));
 
-  const classEnum = await getMafiaEnumData("AscensionClass");
-  const classes = matchAll(classEnum, ENUM_NAME_PATTERN);
+  const classes = (await loadClasses()).data.map((c) => c.name);
   fs.writeFileSync("data/classes.json", JSON.stringify(classes));
 
-  const pathEnum = await getMafiaEnumData("AscensionPath");
-  const paths = matchAll(pathEnum, ENUM_NAME_PATTERN);
+  const paths = (await loadPaths()).data.map((p) => p.name);
   fs.writeFileSync("data/paths.json", JSON.stringify(paths));
 }
 
